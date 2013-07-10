a7e67acd065d071b0647cee95168c47c6cef6177
Paritosh Piplewar <paritosh@passion8.co.in>
2013-07-11 03:30:34 +0530

    make index.html to front page

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var data =  fs.readFileSync('index.html');
+  var str       =  data.toString('utf-8');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
